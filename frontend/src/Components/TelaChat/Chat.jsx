5ff1883d3ba5f46fb124721a37db02ffefe1b2fb
Edson Junior <edsonjn2013@gmail.com>
2024-06-11 02:48:45 -0300

    alterede uri

--- frontend/src/Components/TelaChat/Chat.jsx
+++ frontend/src/Components/TelaChat/Chat.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import './Chat.css';
 
-const socket = io('http://localhost:3001', {
+const socket = io('wss://campus-deploy-api.vercel.app/socket.io/?EIO=4&transport=websocket', {
   transports: ['websocket', 'polling'], // Especifique os transportes
 });
 
